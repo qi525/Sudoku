dc2d29c1f29efcb26348c15f00d7cd59fc091680
qi525 <1037482258@qq.com>
2025-08-06 16:54:18 +0800

    更新 SudokuApp.py 和 .gitignore 文件

--- SudokuApp.py
+++ SudokuApp.py
@@ -29,7 +29,8 @@ class SudokuCanvas(wx.Panel):
         self.sudoku = sudoku = c_sudoku.Sudoku((margin, margin))
 
         # 根据游戏大小重新设定窗体大小
-        self.SetClientSizeWH(sudoku.get_width() + 2 * margin + 160, sudoku.get_height() + 2 * margin)
+        # 将 SetClientSizeWH 替换为 SetClientSize，并传入 wx.Size 对象
+        self.SetClientSize(wx.Size(sudoku.get_width() + 2 * margin + 160, sudoku.get_height() + 2 * margin))
 
         # 计时器
         self.elapsed_time = 0
@@ -52,14 +53,15 @@ class SudokuCanvas(wx.Panel):
 
         # 添加按钮
         bitmap = wx.Bitmap(os.path.normpath("random.png"), wx.BITMAP_TYPE_PNG)
-        self.btn_random = GB.GradientButton(self, -1, bitmap, u"随机游戏", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 3 + (self.sudoku.GRID_WIDTH - 40) / 2), (120, 40))
+        self.btn_random = GB.GradientButton(self, -1, bitmap, "随机游戏", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 3 + (self.sudoku.GRID_WIDTH - 40) // 2), (120, 40))
         bitmap = wx.Bitmap(os.path.normpath("calc.png"), wx.BITMAP_TYPE_PNG)
-        self.btn_auto_calc = GB.GradientButton(self, -1, bitmap, u"自动计算", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 7 + (self.sudoku.GRID_WIDTH - 40) / 2), (120, 40))
+        self.btn_auto_calc = GB.GradientButton(self, -1, bitmap, "自动计算", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 7 + (self.sudoku.GRID_WIDTH - 40) // 2), (120, 40))
         bitmap = wx.Bitmap(os.path.normpath("restart.png"), wx.BITMAP_TYPE_PNG)
-        self.btn_restart = GB.GradientButton(self, -1, bitmap, u"重新开始", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 8 + (self.sudoku.GRID_WIDTH - 40) / 2), (120, 40))
+        self.btn_restart = GB.GradientButton(self, -1, bitmap, "重新开始", (self.sudoku.get_width() + 2 * self.margin, self.margin + self.sudoku.GRID_WIDTH * 8 + (self.sudoku.GRID_WIDTH - 40) // 2), (120, 40))
 
 
-        self.file_cmb = file_cmb = wx.ComboBox(self, wx.NewId(), size = (100, 24), style = wx.CB_READONLY)
+        # 修复 DeprecationWarning: NewId() is deprecated
+        self.file_cmb = file_cmb = wx.ComboBox(self, wx.ID_ANY, size = (100, 24), style = wx.CB_READONLY)
         for file_name in os.listdir("."):
             if file_name.endswith("pzl"):
                 file_cmb.Append(file_name)
@@ -93,25 +95,26 @@ class SudokuCanvas(wx.Panel):
         self.timer.Start(1000)
 
         self.textTimer.SetLabel("00:00:00")
-        self.textTimer.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + (48 - self.textTimer.GetSize()[1]) / 2)) # 这是为了让控件对齐左边的格子
+        self.textTimer.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + (48 - self.textTimer.GetSize()[1]) // 2)) # 这是为了让控件对齐左边的格子
 
-        self.textProgress.SetLabel(u"完成度:")
-        self.textProgress.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + 48 + (48 - self.textProgress.GetSize()[1]) / 2)) # 这是为了让控件对齐左边的格子
+        self.textProgress.SetLabel("完成度:")
+        self.textProgress.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + 48 + (48 - self.textProgress.GetSize()[1]) // 2)) # 这是为了让控件对齐左边的格子
 
-        self.textFile.SetLabel(u"题库：")
-        self.textFile.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + 106 + (48 - self.textProgress.GetSize()[1]) / 2)) # 这是为了让控件对齐左边的格子
-        self.file_cmb.SetPosition((self.sudoku.get_width() + 2 * self.margin + 40, self.margin + 100 + (48 - self.textProgress.GetSize()[1]) / 2)) # 这是为了让控件对齐左边的格子
+        self.textFile.SetLabel("题库：")
+        self.textFile.SetPosition((self.sudoku.get_width() + 2 * self.margin, self.margin + 106 + (48 - self.textProgress.GetSize()[1]) // 2)) # 这是为了让控件对齐左边的格子
+        self.file_cmb.SetPosition((self.sudoku.get_width() + 2 * self.margin + 40, self.margin + 100 + (48 - self.textProgress.GetSize()[1]) // 2)) # 这是为了让控件对齐左边的格子
 
         self.sudoku.init_sudoku(puzzle, self.file_cmb.GetValue())
 
-        self.__parent.SetTitle(u"Sudoku - 第" + str(self.sudoku.get_puzzle_num()) + u"题")
+        self.__parent.SetTitle("Sudoku - 第" + str(self.sudoku.get_puzzle_num()) + "题")
 
 
     def ReBuffer(self):
         #3 创建一个缓存的设备上下文
-        #print "ReBuffer"
+        #print("ReBuffer")
         size = self.GetClientSize()
-        self.__buffer_bitmap = wx.EmptyBitmap(size.width, size.height)
+        # 修复 DeprecationWarning: Call to deprecated item EmptyBitmap. Use :class:`wx.Bitmap` instead
+        self.__buffer_bitmap = wx.Bitmap(size.width, size.height)
         dc = wx.BufferedDC(None, self.__buffer_bitmap)
         dc.Clear()
 
@@ -119,32 +122,32 @@ class SudokuCanvas(wx.Panel):
         self.reReBuffer = False
 
     def OnSize(self, event):
-        #print "OnSize"
+        #print("OnSize")
         self.ReBuffer()
 
 
     def OnLeftUp(self, event):
-        #print "OnLeftUp"
+        #print("OnLeftUp")
         #self.reReBuffer = True
         pass
 
     def OnIdle(self, event):
         #12 空闲时的处理
-        #print "OnIdle"
+        #print("OnIdle")
         if self.reReBuffer:
             self.ReBuffer()
             self.Refresh(False)
 
     def OnPaint(self, event):
         #13 处理一个paint（描绘）请求
-        #print "OnPaint"
-        self.textProgress.SetLabel(u"完成度:" + str(self.sudoku.get_progress()) + u"/" + str(self.sudoku.GRID_NUM ** 2))
+        #print("OnPaint")
+        self.textProgress.SetLabel("完成度:" + str(self.sudoku.get_progress()) + "/" + str(self.sudoku.GRID_NUM ** 2))
         wx.BufferedPaintDC(self, self.__buffer_bitmap)
 
     def OnKeyDown(self, event):
         '''处理键盘事件'''
         keycode = event.GetKeyCode()
-        #print keycode
+        #print(keycode)
         if keycode == wx.WXK_ESCAPE:
             # esc
             sys.exit()
@@ -174,7 +177,7 @@ class SudokuCanvas(wx.Panel):
         if wx.TheClipboard.Open():
             wx.TheClipboard.SetData(self.do)
             wx.TheClipboard.Close()
-            wx.MessageBox(u"已将此题导出到剪贴板", u"导出提示")
+            wx.MessageBox("已将此题导出到剪贴板", "导出提示")
 
 
     def OnPaste(self):
@@ -187,7 +190,7 @@ class SudokuCanvas(wx.Panel):
                 puzzle_str = do.GetText()
                 if puzzle_str.isdigit()                                         \
                         and len(puzzle_str) == c_sudoku.Sudoku.GRID_NUM ** 2:
-                    dlg = wx.MessageDialog(self, u"确定要导入剪贴板中的题局吗?", u"导入确认", wx.YES_NO)
+                    dlg = wx.MessageDialog(self, "确定要导入剪贴板中的题局吗?", "导入确认", wx.YES_NO)
                     if dlg.ShowModal() == wx.ID_YES:
                         self.InitGame(puzzle_str)
                         self.SetFocus()     # 处理完按钮事件后必须将focus交给canvas, 否则无法响应键盘事件
@@ -196,7 +199,7 @@ class SudokuCanvas(wx.Panel):
 
 
     def OnLeftDown(self, event):
-        #print 'LeftDown'
+        #print('LeftDown')
         pos = event.GetPositionTuple()
         self.sudoku.active_grid(pos)
         self.reReBuffer = True
@@ -233,7 +236,7 @@ class SudokuCanvas(wx.Panel):
 
 class SudokuFrame(wx.Frame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, -1, u"Sudoku", pos = (600, 200), size = (200, 200))
+        wx.Frame.__init__(self, parent, -1, "Sudoku", pos = (600, 200), size = (200, 200))
         self.sketch = SudokuCanvas(self, -1)
         self.SetClientSize(self.sketch.GetClientSize())
 
@@ -246,5 +249,3 @@ class SudokuApp(wx.App):
 if __name__ == "__main__":
     app = SudokuApp(False)  # 设置成False可以在命令行窗口输出调试信息
     app.MainLoop()
-
-
